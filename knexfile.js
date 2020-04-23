11fa99f60f532706e95608e7b33d8b42383bee56
Brandon Carr <bmcarr21@me.com>
2020-04-22 21:37:48 -0500

    add production knex connection

--- knexfile.js
+++ knexfile.js
@@ -10,5 +10,9 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/test-fm-signup'
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 
 };
